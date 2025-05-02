7400f961b3768a54a17f3ba829cfb1ca93160fbc
ZhiangWang033 <zhw033@ucsd.edu>
2025-05-01 17:21:49 -0700

    add load design
    Signed-off-by: ZhiangWang033 <zhw033@ucsd.edu>

--- flow/python_read_design.py
+++ flow/python_read_design.py
@@ -3,6 +3,7 @@
 ### Then convert the design into a hypergraph
 ### Then read global placement solution back and run the global placement incrementally
 ### Then perform legalization and detailed placement
+# You can run this script in this manner:  openroad -python python_read_design.py
 
 import sys
 import argparse
@@ -165,6 +166,90 @@ def get_insts(design, inst_map, file_name, vertex_id):
   f.close()     
 
 
+### This function is only for testing purpose
+### Please replace this function with your own function
+def generate_init_placement(design, file_name):
+  f = open(file_name, "w")
+  block = ord.get_db_block()
+  insts = block.getInsts()
+  registers = get_registers(design)
+  for inst in insts:
+    instName = inst.getName()
+    master = inst.getMaster()
+    masterName = master.getName()
+    BBox = inst.getBBox()
+    isMacro = master.isBlock()
+    isSeq = True if instName in registers else False
+    isFixed = True if inst.isFixed() else False
+    lx = BBox.xMin()
+    ly = BBox.yMin()
+    ux = BBox.xMax()
+    uy = BBox.yMax()
+    width = ux - lx
+    height = uy - ly
+    x_center = (lx + ux) / 2
+    y_center = (ly + uy) / 2
+    isFiller = True if master.isFiller() else False
+    isTapCell = True if ("TAPCELL" in masterName or "tapcell" in masterName) else False
+    #isBuffer = 1 if design.isBuffer(master) else 0
+    #isInverter = 1 if design.isInverter(master) else 0
+    if (isFiller == True or isTapCell == True):
+      continue # ignore filler and tap cells
+    f.write(instName + " ")
+    if (isMacro == True or isFixed == True):
+      continue
+    else:
+      f.write(str(int(x_center)) + " ")
+      f.write(str(int(y_center)) + " ")
+    f.write("\n")
+  f.close()     
+
+
+
+
+
+def load_init_placement(file_name):
+  with open(file_name, "r") as f:
+    content = f.read().splitlines()
+  f.close()
+
+  block = ord.get_db_block()
+  for line in content:
+    items = line.split(" ")
+    if len(items) < 3:
+      continue
+
+    instName = items[0]
+    x = int(float(items[1]))
+    y = int(float(items[2]))
+    x = -1
+    y = -1
+
+    # Set the position of the instance    
+    inst = block.findInst(instName)
+    inst.setLocation(x, y)
+
+
+def run_incremental_placement(design):
+  # Configure and run global placement
+  print("###run global placement###")
+  design.evalTclString("global_placement -routability_driven -timing_driven -skip_initial_place -incremental")
+
+  print("Please use the generated 3_3_place_gp.def and 3_3_place_gp.odb files for remaining flows.")
+  design.writeDef("3_3_place_gp.def")
+  design.writeDb("3_3_place_gp.odb")
+
+  # Run initial detailed placement
+  site = design.getBlock().getRows()[0].getSite()
+  max_disp_x = int((design.getBlock().getBBox().xMax() - design.getBlock().getBBox().xMin()) / site.getWidth())
+  max_disp_y = int((design.getBlock().getBBox().yMax() - design.getBlock().getBBox().yMin()) / site.getHeight())
+  print("The following files are just for testing purpose. Please ignore them.")
+  print("###run legalization###")
+  design.getOpendp().detailedPlacement(max_disp_x, max_disp_y, "")
+  
+  design.writeDef("3_5_place_dp.def")
+  design.writeDb("3_5_place_dp.odb")
+
 def get_IO_pins(IO_map, file_name):
   f = open(file_name, "a")
   f.write("Input/Output Pin information (Each line represents one fixed pin: vertex_id, IO_name, IO_type, x_center, y_center):\n")
@@ -218,6 +303,7 @@ def get_basic_info(file_name):
 
 
 if __name__ == "__main__":
+    # You can run this script in this manner:  openroad -python python_read_design.py
     parser = argparse.ArgumentParser(description="Example script to perform global placement initialization using OpenROAD.")
     parser.add_argument("-d", default="ibex", help="Give the design name")
     parser.add_argument("-t", default="nangate45", help="Give the technology node")
@@ -252,8 +338,19 @@ if __name__ == "__main__":
     # get all the connections
     get_connection(large_net_threshold, hg_file_name, IO_map, inst_map)      
 
+    # from here.  You should get the hypergraph file
+    
+    # Please write your own function to generate the initial placement file
+    #init_placement_file = "init_placement.txt"
+    #generate_init_placement(design, init_placement_file)
 
+    # Load the initial placement and run incremental placement    
+    load_init_placement("init_placement_test.txt")
 
+    run_incremental_placement(design)
+
+    print("Please use the generated 3_3_place_gp.def and 3_3_place_gp.odb files for remaining flows.")
+    print("Good luck with your project!")
 
 
 
